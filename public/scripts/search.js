b97fa0d24401799c1751ecc8a28802c393b4111c
Mahumeaw <bhurinat.kan@student.mahidol.edu>
2024-11-01 09:29:21 +0700

    fix: Hyper link for image
    Add hyper link on text of engName for link to fandom gadgets page

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -27,16 +27,18 @@ function populateCards(items) {
   items.forEach((item) => {
     const source = item._source || {};
 
-    const proxyUrl = "http://localhost:3000/api/image-proxy?url=";
+    const proxyUrl = "";   // nothing use because of it don't work, so sad.
     // const proxyUrl = "";
     const imageUrl = `${proxyUrl}${source.image_url}`;
-
+    console.log(imageUrl);
     const cardHTML = `
       <div class="search-result">
         <img src="${imageUrl}" alt="Gadget Image" />
         <div class="content">
+          <a href=${imageUrl} target="_blank">
           <p class="title" style="display: inline">${source.eng_name}</p>
           <p class="title" style="display: inline"> / ${source.jp_name}</p>
+          </a>
           <p class="description">${source.description}</p>
           <p class="episode">Appears In: ${source.appears_in.join(", ")}</p>
         </div>
