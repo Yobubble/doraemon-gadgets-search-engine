3e812aabdbf7fd74d2e084a5923df597c4af035d
Yobubble <thanachot.onl@student.mahidol.edu>
2024-11-02 10:14:18 +0700

    feat: docker

--- configs.js
+++ configs.js
@@ -1,2 +1,6 @@
-export const elasticsearchUrl = "http://localhost:9200";
+import dotenv from "dotenv";
+dotenv.config();
+
+// http://localhost:9200
+export const elasticsearchUrl = process.env.ELS_HOST;
 export const defaultPort = 3000;
