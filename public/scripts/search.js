72753ad9af305f8d80775fb9c255f3ffac15ca94
Yobubble <thanachot.onl@student.mahidol.edu>
2024-11-02 10:10:48 +0700

    feat: docker

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -4,7 +4,7 @@ function getQueryParam(param) {
 }
 console.log(getQueryParam("query"));
 
-fetch("http://localhost:3000/api/queryMock", {
+fetch("http://localhost:3000/api/query", {
   // TODO: [BIGBEE]: Change API URL to "http://localhost:3000/api/query"
   method: "POST",
   headers: {
@@ -27,13 +27,12 @@ function populateCards(items) {
   items.forEach((item) => {
     const source = item._source || {};
 
-    const proxyUrl = "";   // nothing use because of it don't work, so sad.
+    const proxyUrl = ""; // nothing use because of it don't work, so sad.
     // const proxyUrl = "";
     const imageUrl = `${proxyUrl}${source.image_url}`;
     console.log(imageUrl);
     const cardHTML = `
       <div class="search-result">
-        <img src="${imageUrl}" alt="Gadget Image" />
         <div class="content">
           <a href=${imageUrl} target="_blank">
           <p class="title" style="display: inline">${source.eng_name}</p>
