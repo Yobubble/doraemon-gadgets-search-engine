615c447dcab6726474b55f2c3f4ff6c35af7802c
Yobubble <thanachot.onl@student.mahidol.edu>
2024-11-03 19:29:44 +0700

    chore: user-data script

--- deploy/user_data.sh
+++ deploy/user_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install docker
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
-apt-get update
-apt-get install -y docker-ce
+apt update
+apt install -y docker-ce
 usermod -aG docker ubuntu
 
 # Install docker-compose
